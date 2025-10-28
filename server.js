49241650359e6397de2df2cef22e23b405424ef1
Devesh-Pramerica <devesh1@pramericalife.in>
2025-10-28 11:16:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const basicAuth = require("basic-auth");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3010;
 
 // Basic Authentication Middleware
 const auth = (req, res, next) => {
