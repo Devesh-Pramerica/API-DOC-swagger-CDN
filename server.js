d1e458097c8c5db8e7dd32c36eaab2b38a68cd74
Devesh | IT | Pramericalife <P040690@pramericalife.in>
2025-10-28 17:07:11 +0530

    port change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const basicAuth = require("basic-auth");
 const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath();
 
 const app = express();
-const PORT = 3000;
+const PORT = 3010;
 
 // Basic Authentication Middleware
 const auth = (req, res, next) => {
