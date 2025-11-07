9bf6e3d8bbca7d5d65c7609209c7faf6643f4b62
Devesh | IT | Pramericalife <P040690@pramericalife.in>
2025-11-07 16:44:14 +0530

    code change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 3080;
 
 app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Origin', 'https://apidev.pramericalife.in');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 res.setHeader('Access-Control-Allow-Credentials', true);
