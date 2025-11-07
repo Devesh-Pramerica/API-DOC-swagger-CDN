a824566669cd1c60049c9cee58d6766dd3700201
Devesh | IT | Pramericalife <P040690@pramericalife.in>
2025-11-07 16:37:35 +0530

    code change

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const swaggerUiAssetPath = require("swagger-ui-dist").getAbsoluteFSPath();
 const app = express();
 const PORT = 3080;
 
+app.use(function (req, res, next) {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+res.setHeader('Access-Control-Allow-Credentials', true);
+next();
+});
+
 // Basic Authentication Middleware
 const auth = (req, res, next) => {
   const user = basicAuth(req);
